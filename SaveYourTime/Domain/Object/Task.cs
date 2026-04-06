b86bdd24b5a2364e9ad0b6f6f1e9c0549dbe6bdc
Flekseyy <aleksejsmolaninov40@gmail.com>
2026-04-06 21:36:41 +0300

    Add fields for registration
    # Conflicts:
    #	SaveYourTime/SaveYourTime.csproj
    #	SaveYourTime/obj/Debug/net10.0/SaveYourTime.GeneratedMSBuildEditorConfig.editorconfig
    #	SaveYourTime/obj/Debug/net10.0/SaveYourTime.assets.cache
    #	SaveYourTime/obj/SaveYourTime.csproj.nuget.dgspec.json
    #	SaveYourTime/obj/project.assets.json
    #	SaveYourTime/obj/project.nuget.cache
    #	SaveYourTime/obj/project.packagespec.json
    #	SaveYourTime/obj/rider.project.restore.info

---
+++ SaveYourTime/Domain/Object/Task.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Domain;
+
+public class Task
+{
+    
+    public string Id  { get; set; } = string.Empty;
+    public string NameTask  { get; set; } = string.Empty;
+    public string Description  { get; set; } = string.Empty;
+    public string OwnerId  { get; set; } = string.Empty;
+    public string StatusId  { get; set; } = string.Empty;
+    
+}
