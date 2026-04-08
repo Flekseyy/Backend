a1e603a0a97f722b5f5cf66309c8b53bbc4d1466
Flekseyy <aleksejsmolaninov40@gmail.com>
2026-04-08 18:22:15 +0300

    Last last Version

--- SaveYourTime/Domain/Interface/IStatusRepositories.cs
+++
@@ -1,9 +0,0 @@
-﻿using WebApplication1.Domain.Object;
-
-namespace WebApplication1.Domain.Interface;
-
-public interface IStatusRepository
-{
-    Task<IEnumerable<Status>> GetAllAsync();
-    Task<Status?> GetByIdAsync(int id);
-}
