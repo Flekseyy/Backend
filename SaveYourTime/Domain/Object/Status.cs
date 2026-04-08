bd34c59dd2dc5b96b051baf7c22473e4146f92ea
Flekseyy <aleksejsmolaninov40@gmail.com>
2026-04-08 18:22:15 +0300

    Last last Version

--- SaveYourTime/Domain/Object/Status.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1.Domain.Object;                                        
-
-public class Status
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    
-    public ICollection<Assignment> Assignments { get; set; } = new List<Assignment>();
-}
