9950cb35a40b0b6a3f3408f366f1095a6a8691e4
merdan0707 <merdanbebitow07@gmail.ru>
2026-04-14 18:55:49 +0300

    Do CRUD for task

---
+++ SaveYourTime/Domain/Models/AssignmentInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Domain.Models;
+
+public class AssignmentInfo
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    
+    public ICollection<Assignment> Assignments { get; set; } = new List<Assignment>();
+}
