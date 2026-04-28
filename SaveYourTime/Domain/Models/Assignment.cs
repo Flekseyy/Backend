51271e3c34f778b810c063af38fa90a370a8d5a3
Valentine Grachyov <115901713+ValyaGrachyov@users.noreply.github.com>
2026-04-28 21:28:44 +0300

    Do CRUD for Team (#8)
    Co-authored-by: merdan0707 <merdanbebitow07@gmail.ru>

--- SaveYourTime/Domain/Models/Assignment.cs
+++ SaveYourTime/Domain/Models/Assignment.cs
@@ -6,9 +6,11 @@ public class Assignment
     public int UserId { get; set; }
     public int AssignmentStatusId { get; set; }
     public int AssignmentInfoId { get; set; }
+    public int? TeamId { get; set; }          
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    
+
     public User User { get; set; } = null!;
     public AssignmentStatus AssignmentStatus { get; set; } = null!;
     public AssignmentInfo AssignmentInfo { get; set; } = null!;
+    public Team? Team { get; set; }          
 }
