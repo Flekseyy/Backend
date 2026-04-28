c4ee7925719308103cf39f436aff4b918d9d3ce2
Valentine Grachyov <115901713+ValyaGrachyov@users.noreply.github.com>
2026-04-28 21:16:03 +0300

    Пизда 2
    * Add fields for registration
    
    # Conflicts:
    #	SaveYourTime/SaveYourTime.csproj
    #	SaveYourTime/obj/Debug/net10.0/SaveYourTime.GeneratedMSBuildEditorConfig.editorconfig
    #	SaveYourTime/obj/Debug/net10.0/SaveYourTime.assets.cache
    #	SaveYourTime/obj/SaveYourTime.csproj.nuget.dgspec.json
    #	SaveYourTime/obj/project.assets.json
    #	SaveYourTime/obj/project.nuget.cache
    #	SaveYourTime/obj/project.packagespec.json
    #	SaveYourTime/obj/rider.project.restore.info
    
    # Conflicts:
    #	SaveYourTime/obj/Debug/net10.0/SaveYourTime.GeneratedMSBuildEditorConfig.editorconfig
    #	SaveYourTime/obj/Debug/net10.0/SaveYourTime.assets.cache
    #	SaveYourTime/obj/SaveYourTime.csproj.nuget.dgspec.json
    #	SaveYourTime/obj/SaveYourTime.csproj.nuget.g.props
    #	SaveYourTime/obj/SaveYourTime.csproj.nuget.g.targets
    #	SaveYourTime/obj/project.assets.json
    #	SaveYourTime/obj/project.nuget.cache
    #	SaveYourTime/obj/project.packagespec.json
    #	SaveYourTime/obj/rider.project.model.nuget.info
    #	SaveYourTime/obj/rider.project.restore.info
    
    * fields in more detail
    
    * Last Version
    
    * Last last Version
    
    ---------
    
    Co-authored-by: Flekseyy <aleksejsmolaninov40@gmail.com>

---
+++ SaveYourTime/Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication1.Infrastructure.Data;
+using WebApplication1.Domain.Object;
+using Microsoft.EntityFrameworkCore;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Username).IsUnique();
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Email).IsUnique();
+    }
+}
