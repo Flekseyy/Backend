ee088c0796f40069e581b695ccd00899627824ac
Valentine Grachyov <115901713+ValyaGrachyov@users.noreply.github.com>
2026-04-28 21:33:05 +0300

    Done ORM (#10)
    Co-authored-by: Alexsey Smolyaninov <aleksejsmolaninov40@gmail.com>

---
+++ SaveYourTime/Application/DTOs/Inputs/AssignmentInput.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Application.DTOs.Inputs;
+
+public record AssignmentInput(
+    string Title,
+    string? Description,
+    int UserId,
+    int AssignmentStatusId,
+    int? TeamId,
+    DateTime? DueDate
+);
