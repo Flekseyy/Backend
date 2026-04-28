d9adc74bb84a2324347237428112b39a8ed3398b
Valentine Grachyov <115901713+ValyaGrachyov@users.noreply.github.com>
2026-04-28 21:26:23 +0300

    Do CRUD for User (#7)
    # Conflicts:
    #	SaveYourTime/Domain/Models/Assignment.cs
    #	SaveYourTime/Domain/Models/User.cs
    #	SaveYourTime/Infrastructure/Contexts/ApplicationDbContext.cs
    #	SaveYourTime/Infrastructure/Repositories/UserRepositories.cs
    
    Co-authored-by: merdan0707 <merdanbebitow07@gmail.ru>

---
+++ SaveYourTime/Domain/Interfaces/Repositories/ITeamRepository.cs
@@ -0,0 +1,12 @@
+﻿using WebApplication1.Domain.Models;
+
+namespace WebApplication1.Domain.Interfaces.Repositories;
+
+public interface ITeamRepository
+{
+    Task<IEnumerable<Team>> GetAllAsync();
+    Task<Team?> GetByIdAsync(int id);
+    Task<Team> CreateAsync(Team team);
+    Task UpdateAsync(Team team);
+    Task DeleteAsync(int id);
+}
